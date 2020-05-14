7ce2ab325069ba1639d8966c487be186833a0649
Anil Gurung <anil_gurung15@yahoo.com>
2020-05-14 22:24:51 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'artists/new'
+
+  get 'artists/create'
+
+  get 'artists/show'
+
+  get 'artists/edit'
+
+  get 'artists/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
